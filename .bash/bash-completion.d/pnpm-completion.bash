acf7e78492fdf6f40a05dcb734951184a0fd4dac
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2024-03-09 19:09:11 -0500

    Add PNPM bash and zsh completion

---
+++ .bash/bash-completion.d/pnpm-completion.bash
@@ -0,0 +1,8 @@
+# -*- mode: sh; sh-shell: bash -*-
+# vim:ft=sh:
+
+# /bin/echo -e '\033[1;32mIn .bash/bash-completion.d/pnpm-completion.bash\033[0m'
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/bash/__tabtab.bash ] && . ~/.config/tabtab/bash/__tabtab.bash || true
---
+++ .zsh/zsh-completion.d/pnpm-completion.zsh
@@ -0,0 +1,8 @@
+# -*- mode: sh; sh-shell: zsh -*-
+# vim:ft=sh:
+
+# /bin/echo -e '\033[1;32mIn .zsh/zsh-completion.d/pnpm-completion.zsh\033[0m'
+
+# tabtab source for packages
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
